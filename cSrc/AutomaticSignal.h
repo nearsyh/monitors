6abb01318d85c413e76ebb490cf72a68a711fe8a
Frank Song <frank@debian.FrankThinkpad>
2012-11-10 20:35:36 -0500

    clean up again

--- cSrc/AutomaticSignal.h
+++
@@ -1,7 +0,0 @@
-#define AUTOMATIC_SIGNAL uCondition waitList
-#define WAITUNTIL(pred, before, after) (\
-        before\
-        if(!pred) waitList.wait();\
-        waitList.signal();\
-        after)
-#define RETURN(expr) return expr;
